49fbdc3349ec025178d7f0891dbd9648748b049e
Martin Yrjölä <martin.yrjola@gmail.com>
2017-03-29 15:23:07 +0200

    MapView working

--- BeersUnderX.js
+++ BeersUnderX.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+
+import MapView from 'react-native-maps';
 
 export default class BeersUnderX extends Component {
   render() {
     return (
-      <Text>{'\n'}Hello world!</Text>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     );
   }
 }
