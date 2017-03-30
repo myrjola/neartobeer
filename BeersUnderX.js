f9b68d3033b32033fd41408597a201555d6c62e3
Martin Yrjölä <martin.yrjola@gmail.com>
2017-03-30 16:33:38 +0200

    Get marker showing in middle of Stockholm

--- BeersUnderX.js
+++ BeersUnderX.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 
+import View from 'react-native';
+
 import MapView from 'react-native-maps';
+
+const marker = {
+  latlng: {
+    latitude: 59.329323,
+    longitude: 18.068580,
+  }
+};
 
 export default class BeersUnderX extends Component {
   render() {
     return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 59.329323,
-          longitude: 18.068580,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: 59.329323,
+            longitude: 18.068580,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+        <MapView.Marker
+          title="Bar 1"
+          description="Beer under 30"
+          coordinate={marker.latlng}
+          image={require('./images/YellowMug.png')}
+          />
+      </MapView>
     );
   }
 }
