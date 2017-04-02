ddca3f0b1293744f37e5c34d568e847e2ff707c8
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-02 09:54:53 +0200

    Change BarMap to Component class
    Now we can implement componentDidMount to fetch the bars.

--- components/BarMap.js
+++ components/BarMap.js
@@ -1,28 +1,33 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import MapView from 'react-native-maps';
 
 import BarMarker from './BarMarker';
 
-const BarMap = ({ bars }) => (
-  <MapView
-    style={{ flex: 1 }}
-    initialRegion={{
-      latitude: 59.329323,
-      longitude: 18.068580,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }}
-    >
-    {bars.map(bar => (
-      <BarMarker
-        key={bar.post_id}
-        bar={bar}
-        />)
-             )
-    }
-  </MapView>
-);
+class BarMap extends Component {
+  render() {
+    const bars = this.props.bars;
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 59.329323,
+          longitude: 18.068580,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {bars.map(bar => (
+          <BarMarker
+            key={bar.post_id}
+            bar={bar}
+          />),
+                 )
+        }
+      </MapView>
+    );
+  }
+}
 
 BarMap.propTypes = {
   bars: PropTypes.arrayOf(PropTypes.shape({
