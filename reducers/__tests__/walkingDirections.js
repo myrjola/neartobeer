d0790064d0264276ba568506559a5596755f10c2
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-03 16:20:31 +0200

    Add error tests for walkingDirections

--- reducers/__tests__/walkingDirections.js
+++ reducers/__tests__/walkingDirections.js
@@ -4,6 +4,12 @@ import { requestWalkingDirections, receiveWalkingDirections,
          fetchWalkingDirectionsIfNeeded } from '../../actions/walkingDirections';
 import walkingDirections, { initialState } from '../walkingDirections';
 
+var store = null;
+
+beforeEach(() => {
+  store = mockStore({ walkingDirections: initialState });
+});
+
 const origin = {
   latitude: 53.213,
   longitude: 69.123,
@@ -29,12 +35,25 @@ it('receives new walking directions', () => {
   expect(walkingDirections(state, receiveWalkingDirections(origin, coords, destination))).toMatchSnapshot();
 });
 
-const store = mockStore({ walkingDirections: initialState });
+it('errors when HTTP request fails', async () => {
+  fetch.mockResponseFailure('Service unavailable!');
+  navigator.mockSuccess({ coords: origin });
+  await store.dispatch(fetchWalkingDirectionsIfNeeded(destination));
+  expect(store.getActions()).toMatchSnapshot();
+});
 
 it('requests and receives new walking directions', async () => {
   const response = '{"routes": [{"overview_polyline": {"points": "_p~iF~ps|U_ulLnnqC_mqNvxq`@"}}]}';
   fetch.mockResponseSuccess(response);
   navigator.mockSuccess({ coords: origin });
+  await store.dispatch(fetchWalkingDirectionsIfNeeded(destination));
+  expect(store.getActions()).toMatchSnapshot();
+});
+
+it('errors when geolocation fails', async () => {
+  const response = '{"routes": [{"overview_polyline": {"points": "_p~iF~ps|U_ulLnnqC_mqNvxq`@"}}]}';
+  fetch.mockResponseSuccess(response);
+  navigator.mockFailure("Can't get location");
   await store.dispatch(fetchWalkingDirectionsIfNeeded(destination));
   expect(store.getActions()).toMatchSnapshot();
 });
