456549f6f40eb1edd03b3ca6dddbb523b9f0622b
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-03 21:17:08 +0200

    #6 Add tests for reducers/maxBeerPriceCategory

---
+++ reducers/__tests__/maxBeerPriceCategory.js
@@ -0,0 +1,10 @@
+import { setMaxBeerPriceCategory } from '../../actions/maxBeerPriceCategory';
+import maxBeerPriceCategory, { BEER_UNDER_40 } from '../maxBeerPriceCategory';
+
+it('sets max beer price category', () => {
+  expect(maxBeerPriceCategory({}, setMaxBeerPriceCategory(BEER_UNDER_40))).toMatchSnapshot();
+});
+
+// Local Variables:
+// mode: js2-jsx
+// End:
