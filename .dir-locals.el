d17dd93819d724a6a24344d6786727b69111869a
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-05 16:28:09 +0200

    Fix compilation error regexp

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,4 +5,7 @@
   (sgml-basic-offset . 2)
   (js-indent-level . 2)
   (js-switch-indent-offset . 2)
-  (js2-basic-offset . 2)))
+  (js2-basic-offset . 2))
+ (compilation-mode
+  (compilation-error-regexp-alist quote
+                                  (("at .* (\\([_[:alnum:]-/]*.js\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\))$" 1 2 3)))))
