6e1be3a4780d5e68e7d98c012c7a4fba8c88da6a
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-06 16:43:44 +0200

    Extract ZoomToUserLocationButton

--- BeersUnderX.js
+++ BeersUnderX.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, StatusBar, Text, TouchableHighlight } from 'react-native';
+import { View, StyleSheet, StatusBar, Text } from 'react-native';
 import { Provider } from 'react-redux';
 
 import configureStore from './configureStore';
 import FilteredBarMap from './containers/FilteredBarMap';
-import MaxBeerPriceSelector from './containers/MaxBeerPriceSelector.js';
+import MaxBeerPriceSelector from './containers/MaxBeerPriceSelector';
+import ZoomToUserLocationButton from './components/ZoomToUserLocationButton';
 
 const store = configureStore();
 
@@ -19,23 +20,6 @@ const styles = StyleSheet.create({
   },
 });
 
-const DEFAULT_PADDING = { top: 40, right: 40, bottom: 40, left: 40 };
-const COORDINATE1 = {
-  latitude: 59.329323 - 0.01,
-  longitude: 18.068580 - 0.01,
-};
-const COORDINATE2 = {
-  latitude: 59.329323 + 0.01,
-  longitude: 18.068580 + 0.01,
-};
-
-const zoomToUserLocation = () => {
-  this.map.fitToCoordinates([COORDINATE1, COORDINATE2], {
-    edgePadding: DEFAULT_PADDING,
-    animated: true,
-  });
-};
-
 const BeersUnderX = () => (
   <Provider store={store}>
     <View style={StyleSheet.absoluteFillObject}>
@@ -44,9 +28,7 @@ const BeersUnderX = () => (
       <View style={styles.buttonRow}>
         <Text>About</Text>
         <MaxBeerPriceSelector />
-        <TouchableHighlight onPress={zoomToUserLocation}>
-          <Text>Zoom</Text>
-        </TouchableHighlight>
+        <ZoomToUserLocationButton />
       </View>
     </View>
   </Provider>
---
+++ components/ZoomToUserLocationButton.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, StyleSheet, StatusBar, Text, TouchableHighlight } from 'react-native';
+
+const styles = StyleSheet.create({
+});
+
+const DEFAULT_PADDING = { top: 40, right: 40, bottom: 40, left: 40 };
+const COORDINATE1 = {
+  latitude: 59.329323 - 0.01,
+  longitude: 18.068580 - 0.01,
+};
+const COORDINATE2 = {
+  latitude: 59.329323 + 0.01,
+  longitude: 18.068580 + 0.01,
+};
+
+const zoomToUserLocation = () => {
+  this.map.fitToCoordinates([COORDINATE1, COORDINATE2], {
+    edgePadding: DEFAULT_PADDING,
+    animated: true,
+  });
+};
+
+const ZoomToUserLocationButton = () => (
+  <TouchableHighlight onPress={zoomToUserLocation}>
+    <Text>Zoom</Text>
+  </TouchableHighlight>
+);
+
+export default ZoomToUserLocationButton;
+
+// Local Variables:
+// mode: js2-jsx
+// End:
