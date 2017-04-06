de9eced2991d50b8172f5de29ee7f411225b25e3
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-06 10:50:31 +0200

    Add some color

--- components/MaxBeerPriceRadioButtons.js
+++ components/MaxBeerPriceRadioButtons.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { Text, View, TouchableWithoutFeedback } from 'react-native';
 import { RadioButtons } from 'react-native-radio-buttons';
 
+import { BEER_UNDER_30, BEER_UNDER_40, BEER_UNDER_50 } from '../reducers/maxBeerPriceCategory';
+
 const extractNumber = string => string.match(/\d+/)[0];
+
+function getOptionColor(option) {
+  switch (option) {
+    case BEER_UNDER_30:
+      return '#50B07C';
+    case BEER_UNDER_40:
+      return '#FABB3C';
+    default:
+    case BEER_UNDER_50:
+      return '#E45D3E';
+  }
+}
 
 const MaxBeerPriceOption = (option, selected, onSelect, index) => (
   <TouchableWithoutFeedback onPress={onSelect} key={index}>
-    <View>
+    <View style={{ backgroundColor: getOptionColor(option) }}>
       <Text>{extractNumber(option)}kr</Text>
     </View>
   </TouchableWithoutFeedback>
