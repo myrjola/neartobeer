251cbe4f74285b35cad24dccef4d6ca4217794a5
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-06 16:56:24 +0200

    ZoomToUserLocationButton: actually use user location

--- components/ZoomToUserLocationButton.js
+++ components/ZoomToUserLocationButton.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableHighlight } from 'react-native';
+import { Text, TouchableHighlight } from 'react-native';
 
-const DEFAULT_PADDING = { top: 40, right: 40, bottom: 40, left: 40 };
-const COORDINATE1 = {
-  latitude: 59.329323 - 0.01,
-  longitude: 18.068580 - 0.01,
-};
-const COORDINATE2 = {
-  latitude: 59.329323 + 0.01,
-  longitude: 18.068580 + 0.01,
+const boxAroundCoordinates = (position) => {
+  const coordinates = position.coords;
+  const PADDING = 0.01;
+  const upperLeft = {
+    latitude: coordinates.latitude - PADDING,
+    longitude: coordinates.longitude - PADDING,
+  };
+  const bottomRight = {
+    latitude: coordinates.latitude + PADDING,
+    longitude: coordinates.longitude + PADDING,
+  };
+  return [upperLeft, bottomRight];
 };
 
-const zoomToUserLocation = () => {
-  this.map.fitToCoordinates([COORDINATE1, COORDINATE2], {
-    edgePadding: DEFAULT_PADDING,
-    animated: true,
-  });
-};
+const zoomToUserLocation = () => navigator.geolocation.getCurrentPosition(
+  position => this.map.fitToCoordinates(boxAroundCoordinates(position), { animated: true }),
+);
 
 const ZoomToUserLocationButton = () => (
   <TouchableHighlight onPress={zoomToUserLocation}>
