584c12229a312df0fec5a6cf5cfa9554da28a5b6
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-06 10:43:01 +0200

    Almost there

--- components/MaxBeerPriceRadioButtons.js
+++ components/MaxBeerPriceRadioButtons.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
 import { RadioButtons } from 'react-native-radio-buttons';
+
+const extractNumber = string => string.match(/\d+/)[0];
+
+const MaxBeerPriceOption = (option, selected, onSelect, index) => (
+  <TouchableWithoutFeedback onPress={onSelect} key={index}>
+    <View>
+      <Text>{extractNumber(option)}kr</Text>
+    </View>
+  </TouchableWithoutFeedback>
+);
 
 const MaxBeerPriceRadioButtons = props => (
   <RadioButtons
     {...props}
+    renderOption={MaxBeerPriceOption}
     renderContainer={RadioButtons.renderHorizontalContainer}
   />
 );
