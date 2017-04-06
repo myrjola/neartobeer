17d9fbcc2cc735c41f5b4f3dae4b6eb2907150b3
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-06 11:42:40 +0200

    Style support

--- components/MaxBeerPriceRadioButtons.js
+++ components/MaxBeerPriceRadioButtons.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 import { RadioButtons } from 'react-native-radio-buttons';
 
 import { BEER_UNDER_30, BEER_UNDER_40, BEER_UNDER_50 } from '../reducers/maxBeerPriceCategory';
 
+const styles = StyleSheet.create({
+  option: {
+  },
+});
+
 const extractNumber = string => string.match(/\d+/)[0];
-
-const MaxBeerPriceOption = (option, selected, onSelect, index) => (
-  <TouchableWithoutFeedback onPress={onSelect} key={index}>
-    <View style={{ backgroundColor: selected ? getOptionColor(option) : 'transparent' }}>
-      <Text>{extractNumber(option)}kr</Text>
-    </View>
-  </TouchableWithoutFeedback>
-);
-
-const MaxBeerPriceRadioButtons = props => (
-  <RadioButtons
-    {...props}
-    renderOption={MaxBeerPriceOption}
-    renderContainer={RadioButtons.renderHorizontalContainer}
-  />
-);
 
 function getOptionColor(option) {
   switch (option) {
@@ -34,6 +23,28 @@ function getOptionColor(option) {
   }
 }
 
+const getOptionStyle = (option, selected) => {
+  const backgroundColor = selected ? getOptionColor(option) : 'transparent';
+  return StyleSheet.flatten([styles.option, { backgroundColor }]);
+};
+
+const MaxBeerPriceOption = (option, selected, onSelect, index) => (
+  <TouchableWithoutFeedback onPress={onSelect} key={index}>
+    <View
+      style={getOptionStyle(option, selected)}
+    >
+      <Text>{extractNumber(option)}kr</Text>
+    </View>
+  </TouchableWithoutFeedback>
+);
+
+const MaxBeerPriceRadioButtons = props => (
+  <RadioButtons
+    {...props}
+    renderOption={MaxBeerPriceOption}
+    renderContainer={RadioButtons.renderHorizontalContainer}
+  />
+);
 
 export default MaxBeerPriceRadioButtons;
 
