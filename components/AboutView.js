c5425ff2ca66beb7d7ece5fa8c9e81f8a1f9d002
Martin Yrjölä <martin.yrjola@gmail.com>
2017-04-23 12:45:47 +0200

    Extract navBarHeight

--- components/AboutView.js
+++ components/AboutView.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { navBarHeight } from '../constants';
 
 const styles = StyleSheet.create({
   appView: {
     flex: 1,
     backgroundColor: '#fcbb3a',
-    // There is a NavBar on top. This was the best way to not get overlapped by
-    // it https://github.com/aksonov/react-native-router-flux/issues/1279#issuecomment-253459678
-    ...Platform.select({
-      ios: {
-        top: 64,
-      },
-      android: {
-        top: 54,
-      },
-    }),
+    top: navBarHeight,
   },
   title: {
     textAlign: 'center',
--- constants.js
+++ constants.js
@@ -1,2 +1,9 @@
+import { Platform } from 'react-native';
+
 export const borderColor = '#BBB';
 export const backgroundColor = '#EFEFF2';
+
+export const navBarHeight = Platform.select({
+  ios: 64,
+  android: 54,
+});
