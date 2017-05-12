ba53ed16db890af4734659ef8c4596b3245dcc7c
derek <derek.cardol@gmail.com>
2017-05-12 16:26:50 +0200

    Added user feedback

--- components/AboutView.js
+++ components/AboutView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Alert, Button, Text, View } from 'react-native';
 import { navBarHeight } from '../constants';
 
 const styles = StyleSheet.create({
@@ -16,20 +16,73 @@ const styles = StyleSheet.create({
   subtitle: {
     textAlign: 'center',
   },
+  textinput: {
+    backgroundColor: '#ffffff',
+    padding: 10,
+    marginHorizontal: 20,
+    borderRadius: 5,
+    marginVertical: 20,
+    height: 100,
+    textAlignVertical: 'top',
+  },
+  button: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
 });
 
-const AboutView = () => (
-  <View style={styles.appView} accessibilityLabel="About the service">
-    <View style={styles.text}>
-      <Text style={styles.title}>
-        Neartobeer
-      </Text>
-      <Text style={styles.subtitle}>
-        Your best bet to get a cheap beer nearby
-      </Text>
-    </View>
-  </View>
-);
+class AboutView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
+    this.onButtonPress = this.onButtonPress.bind(this);
+  }
+
+  onButtonPress() {
+    const body = `message=${this.state.text}`;
+    fetch('http://n2bmail.lsrodrigues.com/mail.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body,
+    });
+    Alert.alert('Message sent, thank you!');
+    this.text.clear();
+  }
+
+  render() {
+    return (
+      <View style={styles.appView} accessibilityLabel="About the service">
+        <Text style={styles.title}>
+                Neartobeer
+            </Text>
+        <Text style={styles.subtitle}>
+                Your best bet to get a cheap beer nearby
+            </Text>
+        <TextInput
+          style={styles.textinput}
+          placeholder="Send us bar/price updates or feedback!"
+          multiline={true}
+          underlineColorAndroid="transparent"
+          ref={(el) => { this.text = el; }}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+        />
+        <View style={styles.button}>
+          <Button
+            onPress={this.onButtonPress}
+            title="Send"
+            accessibilityLabel="Send feedback or update bar info"
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
 
 export default AboutView;
 
